3ad6e748bf7379cd4256c2981b2ab04151cb366a
Luci-Rhys <155256360+Luci-Rhys@users.noreply.github.com>
2024-09-23 15:05:48 -0500

    added comments to file with standard heading and explanation of what the program does

---
+++ maze_solver.py
@@ -0,0 +1,6 @@
+# Calen Jones
+# Dr. Nicholson
+# Maze Solver (Assignment 1)
+# CSCI 4560-19
+# The objective of this maze solver program is to "read" a maze map from a file, find a path from the source to the exit,
+# and display this map and newfound path to the console
